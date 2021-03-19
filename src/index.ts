8bb3cc48198dd5bca306606b0e35fa061845934c
Jyotinder Singh <jyotinder.singh@cloudera.com>
2021-03-19 14:24:31 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import * as Express from "express";
+import { buildSchema, Query, Resolver } from "type-graphql";
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String, {
+    nullable: true,
+    description: "Just a description of the API",
+  })
+  async helloWorld() {
+    return "Hello, World!";
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = Express();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started on http://localhost:4000/graphql");
+  });
+};
+
+main();
