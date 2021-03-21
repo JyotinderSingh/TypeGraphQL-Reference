53fe05e1ad22113bea5515f081fc67474806a634
Jyotinder Singh <jyotinder.singh@cloudera.com>
2021-03-21 18:02:24 +0530

    added authorization

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -1,12 +1,14 @@
-import { Arg, Authorized, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
 import bcrypt from "bcryptjs";
 import { User } from "../../entity/User";
 import { RegisterInput } from "./register/RegisterInput";
+import { isAuth } from "../middleware/isAuth";
+import { logger } from "../middleware/logger";
 
 @Resolver()
 export class RegisterResolver {
   // GraphQL sometimes gets cranky when you dont have a single query in your whole schema, so we'll just keep this one here
-  @Authorized()
+  @UseMiddleware(isAuth, logger)
   @Query(() => String)
   async hello() {
     return "Hello, World!";
@@ -14,7 +16,7 @@ export class RegisterResolver {
 
   @Mutation(() => User)
   async register(
-    @Arg("data") {email, firstName, lastName, password}: RegisterInput,
+    @Arg("data") { email, firstName, lastName, password }: RegisterInput
   ): Promise<User> {
     const hashedPassword = await bcrypt.hash(password, 12);
 
